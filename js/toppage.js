a10adaa4b50fb64def76470839f2e6eb8fa35dd1
Hayato030835 <haya-boyo-soccer030835@ezweb.ne.jp>
2021-05-24 00:55:00 +0900

    お酒説明のアニメーション追加、CSSファイル分割

--- js/toppage.js
+++ js/toppage.js
@@ -1,5 +1,44 @@
 // アニメーション
 $(function () {
+    // 端末判定
+    // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+    const ua = navigator.userAgent;
+    let deviceFlg = 0;
+    if (navigator.userAgent.indexOf('iPhone') > 0 || navigator.userAgent.indexOf('Android') > 0 && navigator.userAgent.indexOf('Mobile') > 0) {
+        // スマートフォン向けの記述
+        deviceFlg = 0;
+    } else if (navigator.userAgent.indexOf('iPad') > 0 || navigator.userAgent.indexOf('Android') > 0) {
+        // タブレット向けの記述
+        deviceFlg = 1;
+    } else {
+        // PC向けの記述
+        deviceFlg = 3;
+    }
+    // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+    // 画面に入ってすぐのアニメーション
+    // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+    const wHeight = $(window).height();
+    const scrollAmount = $(window).scrollTop();
+    if(deviceFlg == 3){
+        $('.alcohol_introduction_area').each(function () {
+            const targetPosition = $(this).offset().top;
+            if(scrollAmount > targetPosition - wHeight + 60) {
+                $(this).addClass("fadeInDown");
+            }
+        });
+    }
+    else{
+        $('.alcohol_introduction_area_media').each(function () {
+            const targetPosition = $(this).offset().top;
+            if(scrollAmount > targetPosition - wHeight + 60) {
+                $(this).addClass("fadeInDown");
+            }
+        });
+    }
+    
+    // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
     $(window).scroll(function () {
         const wHeight = $(window).height();
         const scrollAmount = $(window).scrollTop();
